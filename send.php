249b0481ba6e23a71c3f1d382677c122175ae17b
Danich7 <55358164+Danich7@users.noreply.github.com>
2020-03-03 16:01:41 +0300

    Add files via upload

--- send.php
+++ send.php
@@ -1,16 +1,4 @@
-<html>
-<head>
-<title>Форма заявки с сайта</title>
-</head>
-<body>
 <?php
-if(!isset($_POST['name']) and !isset($_POST['phone'])){
-  ?> <form action="send.php" method="POST">
-  <input class="name" type="text" name="name" placeholder="Ваше имя" required>
-  <input class="phone" type="text" name="phone" placeholder="Телефон" required>
-  <input class="order__button" type="submit" value="Отправить">     <!---СДЕЛАТЬ ОТПРАВКУ НА ПОЧТУ-->
-</form> <?php
-} else {
 $name = $_POST['name'];
 $phone = $_POST['phone']; 
 $name = htmlspecialchars($name);
@@ -20,12 +8,7 @@ $phone = urldecode($phone);
 $name = trim($name);
 $phone = trim($phone);
 if (mail("Danich132@yandex.ru", "Заказ с сайта", "ФИО:".$name.". Phone: ".$phone ,"From: info@danich7.github.io \r\n"))
- {
-    echo "сообщение успешно отправлено";
+ {    echo "сообщение успешно отправлено";
   } else {
     echo "при отправке сообщения возникли ошибки";
-  }
-}
-?>
-</body>
-</html>
+  }?>
